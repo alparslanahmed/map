c4eb14c15a13c0f50c465961b50679b60f9a39d8
Ahmed Alparslan Özdemir <alparslanahmed@gmail.com>
2020-08-16 04:02:51 +0300

    add server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/map'));
+app.use(express.static('./www'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/map/'}),
+    res.sendFile('index.html', {root: 'www/'}),
 );
 
 app.listen(process.env.PORT || 8080);
